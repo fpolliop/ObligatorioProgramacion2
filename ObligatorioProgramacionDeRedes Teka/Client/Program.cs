ae408935010ad4927a7c47dcfda811c1003d9de7
Andres Della Cella <andresdellacella@hotmail.com>
2018-09-25 14:29:27 -0300

    hola

--- ObligatorioProgramacionDeRedes Teka/Client/Program.cs
+++ ObligatorioProgramacionDeRedes Teka/Client/Program.cs
@@ -15,6 +15,7 @@ namespace Client
 
         public static void Main(string[] args)
         {
+            //teka
             ClientController clientController = new ClientController();
             //debeeria hablar con clientController. no con clientProtocol
             Console.WriteLine("Conectando al servidor...");
