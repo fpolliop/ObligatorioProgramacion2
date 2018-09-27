e5e8b5ee11efba86f23ec6e351721c4add79f3a6
LAPTOP-PEB1IAT9\Francisco <fpolliop@gmail.com>
2018-09-27 17:29:35 -0300

    Board

---
+++ Prog d eRedes/ObligatorioProgramacionDeRedes/Game/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Player
+    {
+        private readonly int MonsterPowerAttack = 10;
+        private readonly int SurvivorPowerAttack = 5;
+        private readonly int MonsterHealth = 100;
+        private readonly int SurvivorHealth = 20;
+
+        public string Nickname { get; set; }
+        public string Avatar { get; set; }
+        public Role Role { get; set; }
+        public int Health { get; set; }
+        public int Movements { get; set; }
+
+        public Player(string nickname, Role role)
+        {
+            this.Nickname = nickname;
+            this.Role = role;
+            if (role.Equals(Role.Monster))
+            {
+                this.Health = MonsterHealth;
+            }
+            else
+            {
+                this.Health = SurvivorHealth;
+            }
+        }
+
+        public int GetPowerAttack()
+        {
+            if (Role.Equals(Role.Monster))
+            {
+                return MonsterPowerAttack;
+            }
+            else
+            {
+                return 5;
+            }
+        }
+    }
+}
+
+public enum Role
+{
+    Monster = 0,
+    Survivor = 1
+}
---
+++ Prog d eRedes/ObligatorioProgramacionDeRedes/Game/SlasherMatch.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class SlasherMatch
+    {
+        List<Player> playersInMatch;
+        int round;
+
+        Player[,] board;
+
+        Stopwatch gameTime = new Stopwatch();
+        public bool MatchHasFinished { get; set; }
+
+        public SlasherMatch()
+        {
+            board = new Player[8, 8];
+            round = 1;
+        }
+        public void StartMatch()
+        {
+            MatchHasFinished = false;
+            gameTime.Start();
+            Thread timerThread = new Thread(() => ControllTimer());
+        }
+
+        private void ControllTimer()
+        {
+            while(gameTime.Elapsed.Minutes < 3)
+            {
+                
+            }
+            gameTime.Stop();
+            FinishMatch();
+        }
+
+        private void FinishMatch()
+        {
+            MatchHasFinished = true;
+        }
+
+        public void AddPlayer(Player newPlayer)
+        {
+            playersInMatch.Add(newPlayer);
+
+            Random random = new Random();
+            int playerRow = -1;
+            int playerColumn = -1;
+            while(!PositionIsValid(playerRow, playerColumn))
+            {
+                playerRow = random.Next(0, 7);
+                playerColumn = random.Next(0, 7);
+            }
+            
+        }
+
+        public bool PositionIsValid(int playerRow, int playerColumn)
+        {
+            if (playerRow.Equals(-1) || playerColumn == -1)
+                return false;
+            if (board[playerRow, playerColumn] != null)
+                return false;
+            return true;
+        }
+
+        public void HandlePlayerAction()
+        {
+
+        }
+    }
+}
+
+public enum PlayerGameAction
+{
+    MoveUp,
+    MoveRight,
+    MoveDown,
+    MoveLeft,
+    AttackUp,
+    AttackRight,
+    AttackDown,
+    AttackLeft
+}
+
