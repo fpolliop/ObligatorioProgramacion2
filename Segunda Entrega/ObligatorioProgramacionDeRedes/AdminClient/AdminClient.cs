26c62251b078c29c1ad29319a32b21634e0b5a8e
LAPTOP-PEB1IAT9\Francisco <fpolliop@gmail.com>
2018-11-09 15:51:16 -0300

    remoting - no funcionando aun

---
+++ Segunda Entrega/ObligatorioProgramacionDeRedes/AdminClient/AdminClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminClient
+{
+    class AdminClient
+    {
+        static void Main(string[] args)
+        {
+            CreateRemotingInfrastructure();
+
+        }
+
+        private static void CreateRemotingInfrastructure()
+        {
+            IDictionary props = new Hashtable();
+            props["port"] = 0;
+            BinaryServerFormatterSinkProvider serverProvider = new BinaryServerFormatterSinkProvider();
+            new TcpChannel(props, null, serverProvider);
+            serverProvider.TypeFilterLevel = TypeFilterLevel.Full;
+            TcpChannel chan = new TcpChannel(props, null, serverProvider);
+            ChannelServices.RegisterChannel(chan, false);
+        }
+    }
+}
---
+++ Segunda Entrega/ObligatorioProgramacionDeRedes/Common/IAdminAccess.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Drawing;
+namespace Common
+{
+    public interface IAdminAccess
+    {
+        void AddPlayer(string nickName, Image avatar);
+    }
+}
